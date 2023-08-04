0527b939c5b4d0d17625a5fc1bc09dd50b0dff29
isinhag <isabeladasilvagomes@gmail.com>
2023-08-04 16:12:17 -0300

    string

---
+++ exemplos/2_TIPOS_DE_DADOS/1_number/script.js
@@ -0,0 +1,8 @@
+console.log(5);
+console.log(35.7);
+console.log(-76);
+console.log(100.45);
+console.log(typeof 5);
+console.log(typeof 35.7);
+console.log(typeof -76);
+console.log(typeof 100.45);
---
+++ exemplos/2_TIPOS_DE_DADOS/4_string/script.js
@@ -0,0 +1,11 @@
+console.log("Meu nome é Isabela.")
+console.log('Desenvolvimento é no Senai')
+
+// Template literals (crase) ao lado da tecla P
+// Usar Shit
+
+console.log(`teste`)
+
+console.log(typeof "Infinity")
+console.log(typeof Infinity )
+
